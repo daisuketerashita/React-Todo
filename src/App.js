20610c917adf87ca74eed40da2dcf847893e271a
寺下大典 <ekusiad.tera@icloud.com>
2022-07-27 20:06:43 +0900

    フォームの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import ToDoList from "./ToDoList";
+
 function App() {
   return (
     <div>
       <ToDoList />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>タスクの削除</button>
+      <div>残りのタスク：0</div>
     </div>
   );
 }
